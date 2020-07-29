a2f26ff0c28d15add35acc9eda35d4b02d602947
Srikar KSV <srikar1awesome@gmail.com>
2020-07-29 19:42:25 +0530

    Made changes to mail error to MODS if data📦 not recieved.
    Also an alternative HTML stucture added.

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from stats_extractor import get_all_cases
 import smtplib
 from email.message import EmailMessage
 from plaintext import plain_text
+from formatted_html import html_format
 
 app = Flask(__name__)
 
@@ -15,18 +16,33 @@ msg["From"] = f'Covid-19 stats <{email}>'
 msg['To'] = 'srikar1awesome@gmail.com'
 
 
+def error_occured(error):
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as stmp:
+        msg['Subject'] = 'Alert📢 something wrong happened'
+        msg['To'] = 'srikar1awesome@gmail.com'
+        msg.set_content("The error:", error)
+        stmp.login(email, password)
+        stmp.send_message(msg)
+
+
 @app.route('/')
 def home():
     daily_cases_url = 'https://api.covid19india.org/data.json'
     india_all_cases = 'http://covid-19india-api.herokuapp.com/all'
     golabl_cases_url = 'http://covid-19india-api.herokuapp.com/global'
 
-    cases_data = get_all_cases(
-        daily_cases_url, india_all_cases, golabl_cases_url)
-
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as stmp:
-        text = plain_text(cases_data)
-        msg.set_content(text)
+        try:
+            cases_data = get_all_cases(
+                daily_cases_url, india_all_cases, golabl_cases_url)
+            text = plain_text(cases_data)
+            msg.set_content(text)
+            msg.add_alternative(html_format(cases_data), subtype="html")
+        except Exception as e:
+            error_occured(e)
+            text = "We are sorry dear consumer due to some technical difficulties we are not able to deliver your updates today."
+            msg.set_content(text)
+
         stmp.login(email, password)
 
         stmp.send_message(msg)

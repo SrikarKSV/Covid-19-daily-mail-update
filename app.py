cfedbf191e516f95e3bb769dc1ac6e8ea3b997e3
Srikar KSV <srikar1awesome@gmail.com>
2020-07-29 17:34:04 +0530

    Added the structure for the plain📖 text to be mailed✉.
    Formatted the numbers to show properly with commas.

--- app.py
+++ app.py
@@ -9,9 +9,43 @@ with open('./login_details.txt') as f:
     email, password = f.read().split()
 
 msg = EmailMessage()
-msg['Subject'] = 'Daily updates of the Corona cases'
+msg['Subject'] = 'Daily updates of the Covid-19 cases'
 msg["From"] = f'Covid-19 stats <{email}>'
 msg['To'] = 'srikar1awesome@gmail.com'
+
+
+def plain_text(cases_data):
+    msg = f"""
+    Daily updates for Covid-19 cases:
+        Country: India
+            Last 24 hours:
+            Confirmed cases in last 24 hours:   {int(cases_data['dailyconfirmed']):,}
+            Recoveries in last 24 hours:    {int(cases_data['dailyrecovered']):,}
+            Deaths in last 24 hours:    {int(cases_data['dailydeceased']):,}
+
+            Total:
+            Total cases:    {int(cases_data['totalconfirmed']):,}
+            Active cases:   {int(cases_data['active_cases']):,}
+            Active cases rate:   {cases_data['active_rate']}
+            Total recoveries:   {int(cases_data['totalrecovered']):,}
+            Recovery rate:   {cases_data['recovered_rate']}
+            Total deaths:   {int(cases_data['totaldeceased']):,}
+            Death rate:     {cases_data['death_rate']}
+
+            For more details visit: https://www.covid19india.org/
+
+        Country: Global
+            Total confirmed cases:   {int(cases_data['global_confirmed_cases']):,}
+            
+            Total active cases:     {int(cases_data['global_active_cases']):,}
+            Active rate:    {cases_data['global_active_rate']}
+            Total recoveries:   {int(cases_data['global_recovered_cases']):,}
+            Recovery rate:   {cases_data['global_recovered_rate']}
+            Total deaths:   {int(cases_data['global_death_cases']):,}
+            Death rate:     {cases_data['global_death_rate']}
+    """
+
+    return msg
 
 
 @app.route('/')
@@ -24,5 +58,14 @@ def home():
         daily_cases_url, india_all_cases, golabl_cases_url)
 
     with smtplib.SMTP_SSL('smtp.gmail.com', 465) as stmp:
-        msg.set_content('Cases')
+        text = plain_text(cases_data)
+        msg.set_content(text)
         stmp.login(email, password)
+
+        stmp.send_message(msg)
+
+    return "<h1>Done<h1>"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
